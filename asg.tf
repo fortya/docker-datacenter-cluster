eddd2ec4dfeb82fdbb2b77425d5e61f323f60f5b
Moe Sy <7231851+mokio8@users.noreply.github.com>
2018-03-24 09:31:26 -1000

    Add asg for dtr nodes

--- asg.tf
+++ asg.tf
@@ -126,6 +126,71 @@ module "node-manager-asg" {
   ]
 }
 
+  module "node-dtr-asg" {
+  source = "terraform-aws-modules/autoscaling/aws"
+
+  name           = "${var.service}-${var.service_instance}-dtr-asg"
+  load_balancers = ["${module.node-dtr-elb.this_elb_id}"]
+
+  user_data = "${data.template_file.node-manager.rendered}"
+  key_name  = "${var.ssh_key_name}"
+
+  lc_name = "${var.service}-${var.service_instance}-manager-lc"
+
+  image_id        = "${data.aws_ami.ubuntu.id}"
+  instance_type   = "t2.medium"
+  security_groups = ["${aws_security_group.node-manager.id}"]
+
+  root_block_device = [
+    {
+      volume_size = "150"
+      volume_type = "gp2"
+    },
+  ]
+
+  asg_name                  = "${var.service}-${var.service_instance}-dtr-asg"
+  vpc_zone_identifier       = ["${module.vpc.public_subnets}"]
+  health_check_type         = "EC2"
+  min_size                  = "${var.dtr_node_min_count}"
+  max_size                  = "${var.dtr_node_max_count}"
+  desired_capacity          = "${var.dtr_node_desired_count}"
+  wait_for_capacity_timeout = 0
+
+  tags = [
+    {
+      key                 = "Provisioner"
+      value               = "Terraform"
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Owner"
+      value               = "${var.service_owner}"
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Stage"
+      value               = "${var.service_stage}"
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Service"
+      value               = "${var.service}"
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Instance"
+      value               = "${var.service}"
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Name"
+      value               = "${var.service}-${var.service_instance}-dtr-"
+      propagate_at_launch = true
+    },
+  ]
+}
+    
+    
 module "node-worker-asg" {
   source = "terraform-aws-modules/autoscaling/aws"
 
