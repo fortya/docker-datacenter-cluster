720c1e7305f802584edac8b2a43e31b0963eeeda
Moe Sy <7231851+mokio8@users.noreply.github.com>
2018-03-24 10:14:31 -1000

    Remove database subnets

--- vpc.tf
+++ vpc.tf
@@ -8,7 +8,6 @@ module "vpc" {
 
   private_subnets  = ["${var.vpc_private_subnets}"]
   public_subnets   = ["${var.vpc_public_subnets}"]
-  database_subnets = ["${var.vpc_database_subnets}"]
 
   enable_nat_gateway           = true
   enable_vpn_gateway           = false
